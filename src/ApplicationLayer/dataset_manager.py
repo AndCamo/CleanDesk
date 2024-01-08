d1c804c850b33a3a7cf696163b8706a22ea2a019
Gennywhitee <genny.ambrosio03@gmail.com>
2024-01-08 11:28:55 +0100

    countTartget

--- src/ApplicationLayer/dataset_manager.py
+++ src/ApplicationLayer/dataset_manager.py
@@ -2,6 +2,10 @@ import pandas as pd
 import os
 
 PATH_SEPARATOR = os.sep
+LABEL_DICTIONARY = {1 : "Society & Culture", 2 : "Science & Mathematics", 3 : "Health", 4 : "Education & Reference", 5 : "Computers & Internet", 
+            6 : "Sports", 7 : "Business & Finance", 8 : "Entertainment & Music", 9 : "Family & Relationships", 10 : "Politics & Government"}
+LABEL_LIST = list(LABEL_DICTIONARY.values())
+
 
 def createShortDataset(rowsNumber):
    dataset = pd.read_csv(f"dataset{PATH_SEPARATOR}train.csv")
@@ -14,3 +18,17 @@ def createShortDataset(rowsNumber):
 
       dataframe = pd.DataFrame(shortDataset, columns=['Class', 'Title', 'Body', 'Answer'])
       dataframe.to_csv(f"dataset{PATH_SEPARATOR}tmp_dataset.csv", encoding='utf-8', index=False, columns=['Class', 'Title', 'Body', 'Answer'])
+
+
+def countTargetByClass(): 
+   tmp_dict = {}
+   for item in LABEL_LIST:
+      tmp_dict.update({item : 0})
+   dataset = pd.read_csv(f"dataset{PATH_SEPARATOR}tmp_dataset.csv")
+   for index, row in dataset.iterrows():
+      tmp_target = LABEL_DICTIONARY[row[0]]
+      tmp_dict[tmp_target] += 1
+   return tmp_dict
+         
+   
+   
--- src/ApplicationLayer/main.py
+++ src/ApplicationLayer/main.py
@@ -4,4 +4,10 @@ import dataset_manager
 # GLOBAL VARIABLES
 PATH_SEPARATOR = os.sep
 
-dataset_manager.createShortDataset(3000)
+#dataset_manager.createShortDataset(3000)
+"""out = dataset_manager.countTargetByClass()
+for item in out:
+    print(str(item)+": "+str(out[item]))"""
+
+dataset = pd.read_csv(f"dataset{PATH_SEPARATOR}tmp_dataset.csv")
+print(dataset.info())
