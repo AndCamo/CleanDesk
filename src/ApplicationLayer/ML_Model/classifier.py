16fa2120f96b7b33048dca3394d670e850c7f3b2
AndCamo <andreacamoia10@gmail.com>
2024-01-08 16:35:38 +0100

    Setup ML Model

---
+++ src/ApplicationLayer/ML_Model/classifier.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import json
+import random
+sys.path.insert(1, f'src{os.sep}ApplicationLayer')
+import dataset_manager
+
+def classify():
+   folderPath = "/Users/andrea/Desktop/TestFolder"
+   log = []
+   for root, dirs, files in os.walk(folderPath):
+      for name in files:
+         if(name[0] != "."):
+            path = os.path.join(root, name)
+            label = random.choice(dataset_manager.LABEL_LIST) 
+            fileInfo  = {"fileName" : name, "filePath" : path, "category" : label}
+            log.append(fileInfo)
+   return json.dumps(log, indent = 4)
+
+
+var = classify()
+
+
+
