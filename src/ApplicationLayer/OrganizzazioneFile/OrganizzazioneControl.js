bdb16915801e44165763bf2a17ced27e03d72e51
Gennywhitee <genny.ambrosio03@gmail.com>
2024-01-10 21:36:21 +0100

    adjustments  in OrganizzazioneControl

--- src/ApplicationLayer/OrganizzazioneFile/OrganizzazioneControl.js
+++ src/ApplicationLayer/OrganizzazioneFile/OrganizzazioneControl.js
@@ -5,16 +5,20 @@ const { FileReportDAO } = require("../../StorageLayer/FileReportDAO");
 const { FileManager } = require("../FileSystemManager/FileManager");
 const path = require("path");
 
+const pathSeparator = path.sep;
+
 class OrganizzazioneControl{
+
+    constructor(){}
 
     creaReportORG(logEntries,initPath){
         // check if the logEntries is not empty
-        if (!logEntries || !Array.isArray(logEntries) || jsonLog.length === 0) {
-            console.error("Invalid jsonLog");
+        if (!logEntries || !Array.isArray(logEntries) || logEntries.length === 0) {
+            console.error("Invalid JSON File");
             return;
         }
         const reportDAO = new ReportORG_DAO();
-        tsDate = new Date();
+        let tsDate = new Date();
 
         //Creating a new temporary ReportBean
         const reportBean = new ReportORGBean(undefined,"","",initPath, tsDate,"Created");
@@ -25,10 +29,10 @@ class OrganizzazioneControl{
         const fileReportDAO = new FileReportDAO();
 
         //For each log in Json file
-        logEntries.forEach(item => {
+        logEntries.forEach((item) => {
 
             //Instantiate a variable representing the final path
-            let finalPath = path.join(initPath,item.category);
+            let finalPath = initPath + pathSeparator + item.category;
 
             //Creating an object FileReport from log
             const fileReportBean = new FileReportBean(reportBean.id, item.fileName, finalPath);
