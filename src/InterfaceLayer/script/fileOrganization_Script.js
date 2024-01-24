42308535e9e9e5bdbf41f5751e769ea9625b6268
AndCamo <andreacamoia10@gmail.com>
2024-01-24 15:52:03 +0100

    Working on new Organization GUI

--- src/InterfaceLayer/script/fileOrganization_Script.js
+++ src/InterfaceLayer/script/fileOrganization_Script.js
@@ -1,5 +1,25 @@
-const { ipcRenderer } = require('electron')
+const { ipcRenderer } = require('electron');
+const { link } = require('original-fs');
 
+const blacklist = [];
+
+window.onload = function() {
+   if (blacklist.length == 0){
+      document.getElementById('blacklist').innerHTML = "VUOTA";
+   } else {
+      list = getBlackListFiles();
+      document.getElementById('blacklist').innerHTML = list;
+   }
+ };
+
+
+function getBlackListFiles(){
+   filePicker = document.getElementById('blacklist-input');
+   list = filePicker.files;
+   console.log(list)
+   document.getElementById('blacklist').innerHTML = list[0].path;
+   return list;
+}
 
 function startOrganization(){
    path = document.getElementById("folderPath").value;
@@ -16,4 +36,5 @@ function startOrganization(){
 function connectionTest(){
    ipcRenderer.send("test");
 }
+
 
